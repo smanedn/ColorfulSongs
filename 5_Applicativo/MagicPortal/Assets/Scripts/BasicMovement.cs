c3eea0485f9b8e77673c00111ae428d1753e3918
Lorenzo Berther <lorenzo.berther@gmail.com>
2025-02-05 13:13:29 +0100

    V0.4.5
    - Cambiato lo script del movimento
    -> Ora si muove correttamente
    -> Si gira verso la direzione in cui si muove

--- 5_Applicativo/MagicPortal/Assets/Scripts/BasicMovement.cs
+++ 5_Applicativo/MagicPortal/Assets/Scripts/BasicMovement.cs
@@ -1,22 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] public float speed = 5.0f;
-    [SerializeField] private Rigidbody rb;
+    CharacterController crct;
+    public float x;
+    public float z;
+    public float speed;
+    public float gravity;
+    public float jump;
+    // Start is called before the first frame update
     void Start()
     {
-        
+        crct = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 position = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        long vel = 1;
 
-        transform.Translate(position);
+        //Quando premo W/S aumenta sia la x che la z
+        var dir = new Vector3(x+z, gravity, z-x);
+
+        if (dir.sqrMagnitude != 0)
+        {
+            //calcola l'angolo di rotazione del personaggio in base ai movimenti, poi cambia da rad2deg
+            var targetAngle = Mathf.Atan2(x, z) * Mathf.Rad2Deg;
+            //ruota il personaggio sull'asse Y verso la direzione in cui si sta muovendo
+            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+            crct.Move(dir * vel * Time.deltaTime);
+        }
     }
 }
