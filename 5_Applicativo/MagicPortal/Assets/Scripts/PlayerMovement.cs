b145322cf129f74ca24e814aa9b60122b6af2d19
Scr1my <scrimy.g@gmail.com>
2025-02-12 10:11:27 +0100

    V0.5.1
    - Modificato il movement del player
    - Sistemato il salto
    - Diviso il codice in metodi
    - Privatizzato le proprietà

---
+++ 5_Applicativo/MagicPortal/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 3.0f;
+    [SerializeField] private float gravity = 9.81f;
+    [SerializeField] private float jumpHeight = 1f;
+    private CharacterController characterController;
+    private float veritcalVelocity;
+    private float x;
+    private float y;
+    private float z;
+    private Vector3 position;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        Movement();
+        Turn();
+    }
+    private void Movement()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        y = VerticalForceCalculation();
+        position = new Vector3(x + z, y, z - x);
+    }
+
+    private void Turn()
+    {
+        characterController.Move(position * speed * Time.deltaTime);
+        if (position.sqrMagnitude != 0)
+        {
+            var targetAngle = Mathf.Atan2(x, z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+            characterController.Move(position * speed * Time.deltaTime);
+        }
+    }
+    private float VerticalForceCalculation()
+    {
+        if (characterController.isGrounded)
+        {
+            veritcalVelocity = -1f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                veritcalVelocity = Mathf.Sqrt(jumpHeight * gravity * 2);
+            }
+        }
+        else
+        {
+            veritcalVelocity -= gravity * Time.deltaTime;
+        }
+        return veritcalVelocity;
+    }
+}
