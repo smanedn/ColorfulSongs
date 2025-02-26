8d5b27f992e191b68f8b32dab374516bb755642d
Lorenzo Berther <lorenzo.berther@gmail.com>
2025-02-26 16:24:52 +0100

    V0.6.8.3
    FUNZIONA TUTTO

--- 5_Applicativo/MagicPortal/Assets/Scripts/RebindControl.cs
+++ 5_Applicativo/MagicPortal/Assets/Scripts/RebindControl.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class VideoOption : MonoBehaviour
+public class RebindControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
