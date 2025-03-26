12ab5b0494bd3c3ef9d0318e17086655ae9e7df0
Kamil17 <siddiqui.kamil2006@gmail.com>
2025-03-26 12:32:13 +0100

    V0.7.4
    Correzioni enigma pedana sul muro, aggiunto la griglia e rimosso il pavimento invisibile sotto.

--- 5_Applicativo/MagicPortal/Assets/Scripts/WallPedanaGenerator.cs
+++ 5_Applicativo/MagicPortal/Assets/Scripts/WallPedanaGenerator.cs
@@ -12,7 +12,9 @@ public class WallPedanaGenerator : MonoBehaviour
     [SerializeField] private int endingZ;     //= 12
     [SerializeField] private int startingY;   //= 0;
 
+    [SerializeField] private GameObject ladder;
     [SerializeField] private GameObject pedana;
+    [SerializeField] private GameObject griglia;
     [SerializeField] private GameObject wall;
     [SerializeField] private GameObject parent;
 
@@ -21,46 +23,55 @@ public class WallPedanaGenerator : MonoBehaviour
         int[] posX = new int[endingX - startingX];
         int[] posZ = new int[endingX - startingX];
 
-        int x = startingX;
         int oldZ = 0;
         bool first = true;
         int i = 0;
-        int z = 0;
-        while (x < endingX)
+        int zBlocco = 0;
+
+        var scala = Instantiate(ladder, new Vector3(startingX-1, startingY+2.5f, endingZ-0.5f), Quaternion.identity);
+        scala.name = "ladder[" + startingX + "; " + endingZ + "]";
+
+        scala.transform.SetParent(parent.transform);
+
+        for (int x = startingX; x < endingX; x++)
         {
             if (first)
             {
-                z = Random.Range(startingZ+1, endingZ);
+                zBlocco = Random.Range(startingZ + 1, endingZ);
                 first = false;
             }
             else
             {
-                Debug.Log(z + ";" + endingZ);
-                Debug.Log(z + ";" + endingZ);
                 do
                 {
-                    z = Random.Range(oldZ - 1, oldZ + 2);
-                } while (z >= endingZ || z <= startingZ);
-                
-
+                    zBlocco = Random.Range(oldZ - 1, oldZ + 2);
+                } while (zBlocco >= endingZ || zBlocco <= startingZ);
             }
-            oldZ = z;
+            oldZ = zBlocco;
 
-            var cube = Instantiate(pedana, new Vector3(x, startingY, z), Quaternion.identity);
-            cube.name = "pedana[" + x + "; " + z + "]";
-            posX[i] = x;
-            posZ[i] = z;
+            for (int z = startingZ; z <= endingZ; z++)
+            {
+                if (z == zBlocco)
+                {
+                    var percorso = Instantiate(pedana, new Vector3(x, startingY, z), Quaternion.identity);
+                    percorso.name = "pedana[" + x + "; " + z + "]";
+                    posX[i] = x;
+                    posZ[i] = z;
 
-            cube.transform.SetParent(parent.transform);
-            i++;
-            x++;
+                    percorso.transform.SetParent(parent.transform);
+                    i++;
+                }
+                var cube = Instantiate(griglia, new Vector3(x, startingY, z), Quaternion.identity);
+                cube.name = "griglia[" + x + "; " + z + "]";
+
+                cube.transform.SetParent(parent.transform);
+            }
         }
-
         for (int j = 0; j < posX.Length; j++)
         {
-            var cube = Instantiate(wall, new Vector3(posX[j], posZ[j], endingZ-0.1f), Quaternion.identity);
+            var cube = Instantiate(wall, new Vector3(posX[j], posZ[j]+1, endingZ - 0.1f), Quaternion.identity);
             var xName = startingX + j;
-            cube.name = "pedanaWall[" + xName + "; " + posX[j] + " ; "+ endingZ + "]";
+            cube.name = "pedanaWall[" + xName + "; " + posX[j] + " ; " + endingZ + "]";
             cube.transform.SetParent(parent.transform);
         }
     }
