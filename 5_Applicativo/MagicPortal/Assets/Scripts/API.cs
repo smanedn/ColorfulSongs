263b1bd76004ee5a7d1694c4510f82b9d73419e4
Lorenzo Berther <lorenzo.berther@gmail.com>
2025-04-02 11:12:35 +0200

    V0.8.3
    - Aggiunta la classe API [WP]
    - Aggiunti i Json delle richieste GET sotto la seguente directory [ColorfulSongs\7_Allegati\Json]

---
+++ 5_Applicativo/MagicPortal/Assets/Scripts/API.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine.Networking;
+using UnityEngine;
+using System.Collections;
+
+public class API : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+
+}
