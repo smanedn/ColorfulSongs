8b8a3fe7f483168adacc1ba152c70f5b037e00a3
Sebastiano <sebastiano.debertoldi@gmail.com>
2025-04-16 09:16:46 +0200

    V2.0.3
    Sistemati i pezzi doppi di codice nella leaderboard

--- 5_Applicativo/leaderboard/application/models/Database.php
+++ 5_Applicativo/leaderboard/application/models/Database.php
@@ -18,18 +18,4 @@ class Database
         $capsule->setAsGlobal();
         $capsule->bootEloquent();
     }
-
-
-//    public static function getConnection(){
-//        if (is_null(self::$_connection)) {
-//            try {
-//                self::$_connection = new \PDO("mysql:host=localhost;dbname=colorfulsongs", USERNAME, PASSWORD);
-//                self::$_connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-//            } catch (\PDOException $e) {
-//                die("Connection failed: " . $e->getMessage());
-//            }
-//        }
-//        return self::$_connection;
-//    }
-
 }
