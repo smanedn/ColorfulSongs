d6fa8c1235323162e2599c5296faf8881f2f0233
Scr1my <scrimy.g@gmail.com>
2025-04-16 09:48:18 +0200

    V0.10.3
    - Nascondo nel Menu il cursore quando si sta usando il PAD

--- 5_Applicativo/MagicPortal/Assets/Scripts/PauseMenu.cs
+++ 5_Applicativo/MagicPortal/Assets/Scripts/PauseMenu.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
@@ -9,6 +10,8 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private GameObject inGameGUI;
     [SerializeField] private static bool isPaused;
     [SerializeField] private AudioSource musicSource;
+
+    private string currentInput;
 
     void Start()
     {
@@ -20,6 +23,19 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Mouse.current != null && Mouse.current.delta.ReadValue().magnitude > 0)
+        {
+            currentInput = "MK";
+        }
+
+        float rightStickHorizontal = Input.GetAxis("Horizontal");
+        float rightStickVertical = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(rightStickHorizontal) > 0.1f || Mathf.Abs(rightStickVertical) > 0.1f)
+        {
+            currentInput = "PAD";
+        }
+
         if (Input.GetKeyUp(KeyCode.Escape) || Input.GetKeyUp(KeyCode.JoystickButton7))
         {
             if (isPaused)
@@ -29,6 +45,18 @@ public class PauseMenu : MonoBehaviour
             else
             {
                 PauseGame();
+            }
+        }
+
+        if (isPaused)
+        {
+            if (currentInput == "MK")
+            {
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.visible = false;
             }
         }
     }
@@ -61,8 +89,4 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene("Main Menu");
     }
-
-    
-
-    
 }
