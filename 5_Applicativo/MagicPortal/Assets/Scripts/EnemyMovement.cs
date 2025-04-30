821603dca332c1803667f3e09374dce392869d3b
Kamil17 <siddiqui.kamil2006@gmail.com>
2025-04-30 10:09:11 +0200

    V0.11.1
    -Movimento del nemico funzionante e casuale
    -Animazioni bacchette work in progress

--- 5_Applicativo/MagicPortal/Assets/Scripts/EnemyMovement.cs
+++ 5_Applicativo/MagicPortal/Assets/Scripts/EnemyMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Threading;
 using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Legacy;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
@@ -10,90 +11,97 @@ public class EnemyMovement : MonoBehaviour
     private float enemyX;
     private float y;
     private float enemyZ;
-    private int startX;
-    private int endX;
-    private int startZ;
-    private int endZ;
+    private float startX;
+    private float endX;
+    private float startZ;
+    private float endZ;
 
-    private float timer = 0f;
-    private int enemyPosition = 0; //0=base 1=in salto 2=arrivato REPEAT
-    private bool moved = false; //0=base 1=in salto 2=arrivato REPEAT
+    private int p1;
+    private int p2 = 0;
+
+    private float p1X;
+    private float p1Z;
+    private float p2X;
+    private float p2Z;
+
+    private int enemyPosition = 0;
 
     private Vector3 position;
-    private float[][] positions;
+    //private float[][] positions;
     [SerializeField] private GameObject generator;
     private void Start()
     {
 
-        startX = generator.GetComponent<TerrainGenerator>().getEndX1();
-        endX = generator.GetComponent<TerrainGenerator>().getStartX2();
-        startZ = 0;
-        endZ = generator.GetComponent<TerrainGenerator>().getEndZ();
+        startX = generator.GetComponent<TerrainGenerator>().getEndX1()+1.5f;
+        endX = generator.GetComponent<TerrainGenerator>().getStartX2()-1.5f;
+        startZ = 0.5f;
+        endZ = generator.GetComponent<TerrainGenerator>().getEndZ()-2;
         enemyX = startX + (endX - startX) / 2;
         enemyZ = endZ / 2 - 0.5f;
         y = 0.5f;
 
-        
-        /**positions[0][0] = startX;
-        positions[0][1] = enemyX;
-        positions[0][2] = endX;
-
-        positions[1][0] = startZ;
-        positions[1][1] = enemyZ;
-        positions[1][2] = endZ;*/
-
         position = new Vector3(enemyX, y, enemyZ);
-        MovementDrum();
-    }
-    void Update()
-    {
-        /*timer = Time.fixedTime;
-        print(timer);
-
-        if (Mathf.RoundToInt(timer) % 6 == 0 && timer!=0)
-        {
-            print(Mathf.RoundToInt(timer) + "%6 = 0");
-            float diff = enemyZ - startZ;
-            position = new Vector3(enemyX, y + 2f, startZ + diff / 2);
-        }
-        else if (Mathf.RoundToInt(timer) % 9 == 0)
-        {
-            print(Mathf.RoundToInt(timer) + "%9 = 0");
-            position = new Vector3(enemyX, y, enemyZ);
-        }
-        else if (Mathf.RoundToInt(timer) % 3 == 0)
-        {
-            print(Mathf.RoundToInt(timer) + "%3 = 0");
-            position = new Vector3(enemyX, y, startZ);
-        }
-
-        GetComponent<Rigidbody>().transform.position = position;*/
+        StartCoroutine(MovementDrum());
     }
 
     IEnumerator MovementDrum()
     {
+        float[,] positions = {
+            {enemyX,enemyZ},
+            {endX,enemyZ},
+            {endX,startZ},
+            {enemyX,startZ},
+            {startX,startZ},
+            {startX,enemyZ},
+            {startX,endZ},
+            {enemyX,endZ},
+            {endX,endZ}
+            };
+
         while (true)
+
         {
-            print("Sono nel while");
-            if (enemyPosition == 0)
+            if(enemyPosition == 0)
             {
-                position = new Vector3(enemyX, y, enemyZ);
+
+                do
+                {
+                    p1 = p2;
+                    p2 = Random.Range(0, 9);
+                } while (p1 == p2);
+
+                p1X = positions[p1,0];
+                p1Z = positions[p1,1];
+
+                p2X = positions[p2,0];
+                p2Z = positions[p2,1];
+                enemyPosition++;
+            }
+            if (enemyPosition == 1)
+            {
+                position = new Vector3(p1X, y, p1Z);
                 enemyPosition++;
                 
             }
-            else if (enemyPosition == 1)
+            else if (enemyPosition == 2 || enemyPosition == 4)
             {
-                float diff = enemyZ - startZ;
-                position = new Vector3(enemyX, y + 2f, startZ + diff / 2);
+                float diffZ = p2Z - p1Z;
+                float diffX = p2X - p1X;
+                position = new Vector3(p1X + diffX/2, y + 2f, p1Z + diffZ / 2);
+                if (enemyPosition == 1)
+                {
+                    enemyPosition++;
+                }
+                else
+                {
+                    enemyPosition = 0;
+                }
+            }
+            else if (enemyPosition == 3)
+            {
+                position = new Vector3(enemyX, y, startZ);
                 enemyPosition++;
             }
-            else if (enemyPosition == 2)
-            {
-                print(Mathf.RoundToInt(timer) + "%3 = 0");
-                position = new Vector3(enemyX, y, startZ);
-                enemyPosition = 0;
-            }
-            print(enemyPosition);
             GetComponent<Rigidbody>().transform.position = position;
             yield return new WaitForSeconds(0.5f);
         }
