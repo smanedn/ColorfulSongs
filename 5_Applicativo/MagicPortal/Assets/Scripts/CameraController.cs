9fe69e028d477382118fba9001bdef12853db84e
Scr1my <scrimy.g@gmail.com>
2025-05-07 08:48:36 +0200

    V9.12.1
    - Bloccata la y della telecamera

--- 5_Applicativo/MagicPortal/Assets/Scripts/CameraController.cs
+++ 5_Applicativo/MagicPortal/Assets/Scripts/CameraController.cs
@@ -5,15 +5,18 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] public GameObject player;
     private Vector3 offset;
+    private float y;
     void Start()
     {
         offset = transform.position - player.transform.position;
+        y = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = player.transform.position + offset;
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
     }
 }
 
